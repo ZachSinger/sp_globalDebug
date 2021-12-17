9fb7bbc347a1a515af4c5ccd8b6ba933fc9258f0
DESKTOP-Q8B9QFN\Owner <zachsinger88@gmail.com>
2021-12-16 21:41:56 -0500

    Added alias methods for sp_Core to hold and execute aliased update methods on Map, Menu or Scene. Can handle running methods before and after the initial aliased update separately

--- sp_Core.js
+++ sp_Core.js
@@ -1,26 +1,35 @@
 let sp_Core = {
-    _sceneBaseUpdates:[],
-    _sceneMenuUpdates:[],
-    _sceneMapUpdates:[],
+    _sceneBaseUpdatesPre:[],
+    _sceneMenuUpdatesPre:[],
+    _sceneMapUpdatesPre:[],
+    _sceneBaseUpdatesPost:[],
+    _sceneMenuUpdatesPost:[],
+    _sceneMapUpdatesPost:[],
     _aliasSceneBase: Scene_Base.prototype.update,
     _aliasSceneMenu: Scene_MenuBase.prototype.update,
     _aliasSceneMap: Scene_Map.prototype.update
 }
 
-sp_Core.addBaseUpdate = function(method, index){
-    let updates = this._sceneBaseUpdates;
+sp_Core.addBaseUpdate = function(method, post, index){
+    let updates = post ? 
+    this._sceneBaseUpdatesPost:
+    this._sceneBaseUpdatesPre;
     
     this.addUpdate(updates, method, index)
 }
 
-sp_Core.addMapUpdate = function(method, index){
-    let updates = this._sceneMapUpdates;
+sp_Core.addMapUpdate = function(method, post, index){
+    let updates = post ? 
+    this._sceneMapUpdatesPost:
+    this._sceneMapUpdatesPre;
     
     this.addUpdate(updates, method, index)
 }
 
-sp_Core.addMenuUpdate = function(method, index){
-    let updates = this._sceneMenuUpdates;
+sp_Core.addMenuUpdate = function(method, post, index){
+    let updates = post ? 
+    this._sceneMenuUpdatesPre:
+    this._sceneMenuUpdatesPost;
 
     this.addUpdate(updates, method, index)
 }
@@ -34,4 +43,72 @@ sp_Core.addUpdate = function(location, method, index){
         location.length;
 
     location.splice(index, 0, method);
+}
+
+sp_Core.removeBaseUpdate = function(method){
+    let updates = '_sceneBaseUpdates';
+    
+    this.removeUpdate(updates, method)
+}
+
+sp_Core.removeMapUpdate = function(method){
+    let updates =  '_sceneMapUpdates';
+
+    this.removeUpdate(updates, method)
+}
+
+sp_Core.removeMenuUpdate = function(method){
+    let updates = '_sceneMenuUpdates'
+
+    this.removeUpdate(updates, method)
+}
+
+sp_Core.removeUpdate = function(locationName, method){
+    let location = this[locationName];
+    
+    this[locationName] = location.filter(item => item != method);
+    
+}
+
+Scene_Base.prototype.update = function(){
+    let thisObject = this;
+
+    sp_Core._sceneBaseUpdatesPre.forEach(
+        item => item.call(thisObject)
+    )
+
+    sp_Core._aliasSceneBase.call(this);
+
+    sp_Core._sceneBaseUpdatesPost.forEach(
+        item => item.call(thisObject)
+    )
+}
+
+
+Scene_Map.prototype.update = function(){
+    let thisObject = this;
+
+    sp_Core._sceneMapUpdatesPre.forEach(
+        item => item.call(thisObject)
+    )
+
+    sp_Core._aliasSceneMap.call(this);
+
+    sp_Core._sceneMapUpdatesPost.forEach(
+        item => item.call(thisObject)
+    )
+}
+
+Scene_MenuBase.prototype.update = function(){
+    let thisObject = this;
+
+    sp_Core._sceneMenuUpdatesPre.forEach(
+        item => item.call(thisObject)
+    )
+
+    sp_Core._aliasSceneMenu.call(this);
+
+    sp_Core._sceneMenuUpdatesPost.forEach(
+        item => item.call(thisObject)
+    )
 }

7b89c1307e96e2e90a07a835bc7a3235e05610d0
DESKTOP-Q8B9QFN\Owner <zachsinger88@gmail.com>
2021-12-20 17:19:42 -0500

    Added setCustomProp function, which allows users to add their own unsupported anim prop, provided it is on the root of the target, like x, y, width, etc are

--- sp_Animations.js
+++ sp_Animations.js
@@ -202,6 +202,25 @@ class sp_Action{
         return this;
     }
 
+    setCustomProp(prop, value, dur, pad){
+        let cache = this.index ? this.getPositionData() : this.animation.initalCache;
+        let target = this.target();
+        if(typeof cache[prop] == 'undefined'){
+            console.log('initializing prop')
+            cache[prop] = target[prop];
+        }
+
+        let profile = {
+        }
+
+        profile[prop] = standardPlayer.sp_Core.plotLinearPath(cache[prop], value, dur, pad)
+
+        this.steps[this.index] = Object.assign({}, this.step(), profile);
+
+        this.dur[this.index] = dur;
+        return this;
+    }
+
     setRepeat(numberOfTimes){
         this.repeat[this.index] = numberOfTimes;
         this.repeatCache[this.index] = numberOfTimes;
@@ -330,3 +349,14 @@ class sp_Action{
 
 }
     
+
+
+function testScript(){
+    window.grph = new PIXI.Graphics();grph.beginFill(0xFFFFFF);grph.drawRect(0, 0, 100, 100);
+    SceneManager._scene.addChild(grph)
+    let anim = standardPlayer.sp_Animations.createAnimation(grph);
+    let action = anim.action(0).moveXY(100, 100, 100, 0).then().setCustomProp("tint", 300, 100, 0).setMasterRepeat(1);
+
+    
+    anim.activate();
+}
